723b459c509b23eca3b5708848ef9f355016f9dd
Tehilla Odjoji <odjojitehilla@gmail.com>
2026-02-18 14:16:09 +0100

    Enhance success page layout and styling

--- src/app/dashboard/domains/success/page.tsx
+++ src/app/dashboard/domains/success/page.tsx
@@ -1,7 +1,8 @@
 export default function success() {
     return (
-        <div className="bg-black-600">
-            <h1 className="text-white-500 font-bold justify-center">Payment Successful</h1>
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="text-white-500 text-5xl font-bold justify-center">Payment Successful</h1>
         </div>
     )
 }
+
