c5a6ecfcb5c7e3b9c631462f65cfdb4424c14f0f
Tehilla Odjoji <odjojitehilla@gmail.com>
2026-02-18 14:00:50 +0100

    Add success page for payment confirmation

---
+++ src/app/dashboard/domains/success/page.tsx
@@ -0,0 +1,7 @@
+export default function success() {
+    return (
+        <div className="bg-black-600">
+            <h1 className="text-white-500 font-bold justify-center">Payment Successful</h1>
+        </div>
+    )
+}
